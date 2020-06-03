8317948f2644352b6a750380648470f4f71960f0
kifah <kifah.andary@gmail.com>
2020-06-03 10:29:02 +0300

    final prototype

--- include/header.php
+++ include/header.php
@@ -1,32 +1,6 @@
 <header id="header">
     <!--header-->
-    <div class="header_top">
-        <!--header_top-->
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="contactinfo">
-                        <ul class="nav nav-pills">
-                            <li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 8</a></li>
-                            <li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="social-icons pull-right">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/header_top-->
+
 
     <div class="header-middle">
         <!--header-middle-->
@@ -35,29 +9,6 @@
                 <div class="col-sm-4">
                     <div class="logo pull-left">
                         <a href="index.php"><img src="images/home/logo.png" alt=""/></a>
-                    </div>
-                    <div class="btn-group pull-right">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-                                USA
-                                <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Canada</a></li>
-                                <li><a href="#">UK</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-                                DOLLAR
-                                <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Canadian Dollar</a></li>
-                                <li><a href="#">Pound</a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
                 <div class="navbar-custom-menu">
@@ -72,9 +23,6 @@
                                 echo "<li><a href=login.php><i class=\"fa fa-user\"></i> Account</a></li>";
                             }
                             ?>
-
-                            <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-                            <li><a href="checkout.php"><i class="fa fa-crosshairs"></i> Checkout</a></li>
                             <li ><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart
                                     <span class="label label-success cart_count">1</span>
                                 </a>
@@ -113,22 +61,7 @@
                     <div class="mainmenu pull-left">
                         <ul class="nav navbar-nav collapse navbar-collapse">
                             <li><a href="index.php" class="active">Home</a></li>
-                            <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
-                                <ul role="menu" class="sub-menu">
-                                    <li><a href="shop.php">Products</a></li>
-                                    <li><a href="product-details.php">Product Details</a></li>
-                                    <li><a href="checkout.php">Checkout</a></li>
-                                    <li><a href="cart.php">Cart</a></li>
-                                    <li><a href="login.php">Login</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                <ul role="menu" class="sub-menu">
-                                    <li><a href="blog.php">Blog List</a></li>
-                                    <li><a href="blog-single.php">Blog Single</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="404.php">404</a></li>
+
                             <li><a href="contact-us.php">Contact</a></li>
                         </ul>
                     </div>
